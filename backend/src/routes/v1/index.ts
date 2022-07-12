49570cf0f92a8c26c07bff49be8be8ce434263d2
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-07-12 19:04:49 -0300

    Adding the users route.

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import contacts from './contacts.route';
+import users from './users.route';
 
 const router = Router();
 
 router.use('/contacts', contacts);
+router.use('/users', users);
 
 export default router;
