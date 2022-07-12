fe5725c310b42ebe4e9e7f5498f9be291d73063d
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-07-12 19:34:42 -0300

    Adding the Cognito settings.

---
+++ frontend/src/app/shared/services/cognito.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
+import { Amplify, Auth } from 'aws-amplify';
+
+// Environments
+import { environment } from '../../../environments/environment';
+
+// Models
+import { UsersModel } from '../../users/users.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CognitoService {
+
+  constructor() {
+    Amplify.configure({
+      Auth: environment.cognito,
+    });
+  }
+
+  public signUp(user: UsersModel): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      Auth.signUp({
+        username: user.email,
+        password: user.password,
+      } as any)
+      .then((result: any) => {
+        observer.next(result);
+        observer.complete();
+      }).catch((error: any) => {
+        observer.error(error);
+      });
+    });
+  }
+
+  public confirmSignUp(user: UsersModel): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      Auth.confirmSignUp(user.email as string, user.code as string)
+      .then((result: any) => {
+        observer.next(result);
+        observer.complete();
+      }).catch((error: any) => {
+        observer.error(error);
+      });
+    });
+  }
+
+  public signIn(user: UsersModel): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      Auth.signIn(user.email as string, user.password as string)
+      .then((result: any) => {
+        observer.next(result);
+        observer.complete();
+      }).catch((error: any) => {
+        observer.error(error);
+      });
+    });
+  }
+
+  public signOut(): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      Auth.signOut()
+      .then((result: any) => {
+        observer.next(result);
+        observer.complete();
+      }).catch((error: any) => {
+        observer.error(error);
+      });
+    });
+  }
+
+  public getUser(): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      Auth.currentUserInfo()
+      .then((result: any) => {
+        observer.next(result);
+        observer.complete();
+      }).catch((error: any) => {
+        observer.error(error);
+      });
+    });
+  }
+
+  public updateUser(user: UsersModel): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      Auth.currentUserPoolUser()
+      .then((cognitoUser: any) => {
+        return Auth.updateUserAttributes(cognitoUser, user);
+      }).then((cognitoUser: any) => {
+        observer.next(cognitoUser);
+        observer.complete();
+      }).catch((error: any) => {
+        observer.error(error);
+      });
+    });
+  }
+
+}
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,6 +4,10 @@
 
 export const environment = {
   production: false,
+  cognito: {
+    userPoolId: 'us-east-1_pvBUG59Lc',
+    userPoolWebClientId: '2c5mv2jgmje9qnm4lk53oak50a',
+  },
 };
 
 /*
