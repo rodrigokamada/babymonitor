b1af277dc96c073fb68958aa63be305233fc818a
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-07-12 20:28:53 -0300

    Adding the user model.

---
+++ frontend/src/app/users/users.model.ts
@@ -0,0 +1,18 @@
+export class UsersModel {
+
+  id?: string;
+  firstLetter?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+  oldPassword?: string;
+  showPassword?: boolean;
+  showOldPassword?: boolean;
+  hasPassword?: boolean;
+  image?: string;
+  locale?: string;
+  status?: string;
+  code?: string;
+  token?: string;
+
+}
