468d52498080406d220eff45833512dd9c793518
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-07-12 20:18:41 -0300

    Adding the navbar.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { TranslocoService } from '@ngneat/transloco';
+import * as AOS from 'aos';
+
+// Services
+import { CognitoService } from './shared/services/cognito.service';
+import { StorageService } from './shared/services/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  user: any;
+  languagesList: any[];
+  toggle: boolean;
+
+  constructor(private router: Router,
+              private spinnerService: NgxSpinnerService,
+              private toastrService: ToastrService,
+              private translocoService: TranslocoService,
+              private cognitoService: CognitoService,
+              private storageService: StorageService) {
+    this.languagesList = [];
+    this.toggle = false;
+
+    AOS.init();
+  }
+
+  ngOnInit(): void {
+    this.storageService.user.subscribe((user: any) => {
+      this.user = user;
+
+      if (this.user && this.user.locale) {
+        this.storageService.setLocale(this.user.locale);
+      }
+    });
+  }
+
+  public clickBrand(): void {
+    if (this.storageService.isAuthenticated()) {
+      this.router.navigate(['/monitors']);
+    } else {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  public changeLanguage(locale: string): void {
+    if (locale) {
+      this.storageService.setLocale(locale);
+    }
+    this.toggle = false;
+  }
+
+  public signOut(): void {
+    this.spinnerService.show();
+
+    this.cognitoService.signOut()
+    .subscribe({
+      next: (success: any) => {
+        this.storageService.clear();
+        this.spinnerService.hide();
+        this.translocoService.selectTranslate('app.signOutSuccess')
+      .subscribe((message: string) => this.toastrService.success(message));
+
+        this.router.navigate(['/home']);
+      }, error: (error: any) => {
+        this.spinnerService.hide();
+        this.translocoService.selectTranslate('error.problem')
+      .subscribe((message: string) => this.toastrService.error(message));
+      },
+    });
+  }
 
 }
