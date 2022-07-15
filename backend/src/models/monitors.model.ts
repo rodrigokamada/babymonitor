8ec4d26ce5d766f6b0b7332cb1d4cbc951258f88
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-07-15 18:25:52 -0300

    Adding the name field.

--- backend/src/models/monitors.model.ts
+++ backend/src/models/monitors.model.ts
@@ -4,23 +4,23 @@ export class MonitorsModel {
 
   id: String|undefined;
   code: String|undefined;
-  userId: String|undefined;
+  name: String|undefined;
 
-  constructor(userId: String) {
+  constructor(name: String) {
     this.id = uuidv4();
     this.code = this.generateCode();
-    this.userId = userId;
+    this.name = name;
   }
 
   private generateCode(): String {
     let code = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < 5; i++) {
       code += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
-    return code;
+    return code.toUpperCase();
   }
 
 }
