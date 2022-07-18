d934e5d60e653244a8e99ba538fbd72e48581be8
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-07-18 08:28:30 -0300

    Adding the viewers route.

---
+++ backend/src/routes/v1/viewers.route.ts
@@ -0,0 +1,36 @@
+import createError from 'http-errors';
+import { Request, Response, Router, NextFunction } from 'express';
+
+import authenticationMiddleware from '../../middlewares/authentication.middleware';
+import logger from '../../utils/logger';
+import { mysql } from '../../utils/mysql';
+import { ViewersModel } from '../../models/viewers.model';
+
+const router = Router();
+
+router.post('/', authenticationMiddleware(), async (req: Request, res: Response, next: NextFunction) => {
+  const { userId } = (req as any);
+
+  let { monitor }: any = req.body;
+
+  try {
+    logger.debug(`Searching the monitor by code [${monitor.code}]`);
+
+    monitor = await mysql.execute(`SELECT M.* FROM monitors M WHERE V.code = '${monitor.code}'`);
+
+    const viewer = new ViewersModel(monitor.id, userId, undefined, undefined);
+
+    logger.debug(`Inserting the viewer [${JSON.stringify(viewer)}]`);
+
+    const result = await mysql.execute(`INSERT INTO viewers (id, monitor_id, user_id) VALUES ('${viewer.id}', '${viewer.monitorId}', '${viewer.userId}')`);
+
+    logger.debug(`Viewer [${JSON.stringify(viewer)}] inserted: ${result}`);
+
+    return res.status(201).json(viewer);
+  } catch (error) {
+    logger.error(`An error occurred while inserting the viewer: ${error}`);
+    return next(createError(500));
+  }
+});
+
+export default router;
