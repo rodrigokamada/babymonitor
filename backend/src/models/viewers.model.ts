6a75b4b1c1d71f21731ac010334caa13a3ae9e61
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-07-18 07:13:53 -0300

    Adding the peerId and socketId fields.

--- backend/src/models/viewers.model.ts
+++ backend/src/models/viewers.model.ts
@@ -5,11 +5,15 @@ export class ViewersModel {
   id: String|undefined;
   monitorId: String|undefined;
   userId: String|undefined;
+  peerId: String|undefined;
+  socketId: String|undefined;
 
-  constructor(monitorId: String, userId: String) {
+  constructor(monitorId: String, userId: String, peerId: String|undefined, socketId: String|undefined) {
     this.id = uuidv4();
     this.monitorId = monitorId;
     this.userId = userId;
+    this.peerId = peerId;
+    this.socketId = socketId;
   }
 
 }
