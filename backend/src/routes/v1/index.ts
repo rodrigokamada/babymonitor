5d400a8e7be8fb0a84b3ebce9c7c148191023c71
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-07-18 08:29:16 -0300

    Adding the viewers route.

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import contacts from './contacts.route';
 import monitors from './monitors.route';
 import users from './users.route';
+import viewers from './viewers.route';
 
 const router = Router();
 
 router.use('/contacts', contacts);
 router.use('/monitors', monitors);
 router.use('/users', users);
+router.use('/viewers', viewers);
 
 export default router;
