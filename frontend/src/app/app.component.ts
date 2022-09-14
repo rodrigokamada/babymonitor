5c2ac88df000f4709f73a689ccee0d190480a5cc
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-09-14 08:27:19 -0300

    Adding the aos settings.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'babymonitor';
+  
+  constructor() {
+    AOS.init();
+  }
+
 }
