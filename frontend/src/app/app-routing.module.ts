67fc1ea8f709a975076af21de53b0939ecfa8bca
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-09-17 09:46:58 -0300

    Adding the invite component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule),
   },
   {
+    path: 'invite',
+    loadChildren: () => import('./invite/invite.module').then(mod => mod.InviteModule),
+  },
+  {
     path: 'monitors',
     loadChildren: () => import('./monitors/monitors.module').then(mod => mod.MonitorsModule),
     canActivate: [ AuthenticationGuard ],
