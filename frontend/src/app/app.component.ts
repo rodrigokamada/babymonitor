06958931ffe6e0a1536c933b33a82dd0b2bd5094
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-09-19 22:29:48 -0300

    Adding the eslint settings.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 import { Router } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
@@ -15,7 +15,7 @@ import { StorageService } from './shared/services/storage.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
 
   user: any;
   languagesList: any[];
