4cb8aad952653c38f86306ad8a822811cbb60ccf
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-09-21 22:20:46 -0300

    Adding the serverless settings.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,64 +2,10 @@
 process.env.NODE_CONFIG_DIR = `${__dirname}/../config`;
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
-import config from 'config';
-import http from 'http';
+import serverless from 'serverless-http';
 
 import logger from './utils/logger';
 import app from './app';
 
-const applicationConfig: any = config.get('application');
-
-function normalizePort(val: string) {
-  const port = parseInt(val, 10);
-
-  if (Number.isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-const port = normalizePort(applicationConfig.port);
-app.set('port', port);
-
-function onError(error: any) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      logger.error(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      logger.error(`${bind} is already in use`);
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-logger.info(`Starting the server on the port [${port}]`);
-const server = http.createServer(app);
-
-function onListening() {
-  const addr: any = server.address();
-  const bind: any = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
-  logger.info(`Listening on ${bind}`);
-}
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+logger.info(`Starting the server`);
+export const handler = serverless(app);
