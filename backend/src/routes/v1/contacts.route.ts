fd3aa5a3ba4b546edff031c2e4dabd209dc4ef88
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-09-22 15:05:48 -0300

    Adding the contacts route.

---
+++ backend/src/routes/v1/contacts.route.ts
@@ -0,0 +1,57 @@
+import { DataStore } from 'aws-amplify';
+import config from 'config';
+import createError from 'http-errors';
+import got from 'got';
+import { Request, Response, Router, NextFunction } from 'express';
+
+import logger from '../../utils/logger';
+import { ContactsModel } from '../../models';
+
+const googleConfig: any = config.get('google');
+
+const router = Router();
+
+router.post('/send', async (req: Request, res: Response, next: NextFunction) => {
+  const { body } = req;
+  logger.info(`Sending the contact [${JSON.stringify(body)}]`);
+
+  try {
+    const url = `${googleConfig.recaptcha.url}?secret=${googleConfig.recaptcha.secretKey}&response=${body.token}`;
+    logger.debug(`Requesting the [POST] method in the url [${url}]`);
+
+    const response: any = await got.post(url, {
+      responseType: 'json',
+      resolveBodyOnly: true,
+    });
+
+    logger.debug(`Response [${JSON.stringify(response)}] from Google`);
+
+    if (!response.success) {
+      return next(createError(500));
+    }
+  } catch (error) {
+    logger.error(`An error occurred while validating the Google token: ${error}`);
+    return next(createError(500));
+  }
+
+  try {
+    const contact = new ContactsModel({
+      subject: body.subject,
+      email: body.email,
+      message: body.message,
+    });
+
+    logger.debug(`Inserting the contact [${JSON.stringify(contact)}]`);
+
+    const result = await DataStore.save(contact);
+
+    logger.debug(`Contact [${JSON.stringify(contact)}] inserted: ${result}`);
+
+    return res.status(201).json(contact);
+  } catch (error) {
+    logger.error(`An error occurred while inserting the contact: ${error}`);
+    return next(createError(500));
+  }
+});
+
+export default router;
