573648ea9cab021251c2902f6910ad5b6b4c6b5c
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-09-24 06:10:50 -0300

    Adding the AWS settings.

--- backend/config/default.js
+++ backend/config/default.js
@@ -11,7 +11,19 @@ module.exports = {
   cors: {
   },
 
+  google: {
+    recaptcha: {
+      url: 'https://www.google.com/recaptcha/api/siteverify',
+      secretKey: process.env.GOOGLE_RECAPTCHA_SECRET_KEY,
+    },
+  },
+
   amplify: {
-  }
+    aws_project_region: 'us-east-1',
+    aws_appsync_graphqlEndpoint: 'https://zhbulmokmbh3rphnr4lrvuwkie.appsync-api.us-east-1.amazonaws.com/graphql',
+    aws_appsync_region: 'us-east-1',
+    aws_appsync_authenticationType: 'API_KEY',
+    aws_appsync_apiKey: process.env.AWS_AMPLIFY_API_KEY,
+  },
 
 };
